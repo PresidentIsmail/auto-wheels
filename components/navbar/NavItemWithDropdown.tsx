48670bf022d4ddc792259dd8b91c7dcc0fc2d866
Ismail <shaikhnagismail@gmail.com>
2023-12-30 04:53:22 +0200

    chore: Add dropdown functionality to NavItemWithDropdown component

--- components/navbar/NavItemWithDropdown.tsx
+++ components/navbar/NavItemWithDropdown.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { NAV_ITEMS, SERVICES_DATA } from "@/constants";
 
 import { ChevronDown } from "lucide-react";
+import Link from "next/link";
 
 interface NavItemWithDropDownProps {
   navItem: (typeof NAV_ITEMS)[number];
@@ -17,39 +18,50 @@ const NavItemWithDropdown: React.FC<NavItemWithDropDownProps> = ({
   toggleServicesDropdown,
   showServicesDropdown,
 }) => {
-
   return (
     <li>
-      <button onClick={toggleServicesDropdown}>
-        <span className="flex items-center ">
+      <button
+        aria-haspopup="true"
+        aria-expanded={showServicesDropdown}
+        onClick={toggleServicesDropdown}
+      >
+        <span className="flex items-center gap-1 ">
           {navItem.label}
-          <ChevronDown className="ml-1" />
+          <ChevronDown
+            className={`${
+              showServicesDropdown ? "transform rotate-180" : ""
+            } transition-transform duration-300`}
+          />
         </span>
       </button>
 
       {/* Dropdown */}
-      {showServicesDropdown && (
-        <div className="px-12 py-8 bg-special absolute top-full translate-y-2 grid grid-cols-3 gap-x-4">
-
+      {showServicesDropdown ? (
+        <nav
+          role="menu"
+          className="px-12 py-8 bg-special absolute left-1/2 w-max rounded-md -translate-x-1/2  top-full translate-y-2 grid grid-cols-3 lg:grid-cols-4 gap-2 lg:gap-4"
+        >
           {/* Dropdown Items */}
           {SERVICES_DATA.map((service) => (
-            <div key={service.title} className="mb-4">
-              <h3 className="text-white font-bold mb-2">{service.title}</h3>
-              <ul className="text-white/80">
+            <Link
+              key={service.title}
+              // href={`/services/${service.title.toLowerCase()}`}
+              href={`#`}
+              role="menuitem"
+              className="flex flex-col gap-2 max-w-[224px] w-full hover:bg-white/10 focus-visible:bg-white/10 focus-visible:outline-none px-3 py-3 rounded-md transition-colors "
+            >
+              <h3 className="text-white font-bold">{service.title}</h3>
+              <ul className="text-white/80 text-sm flex flex-col gap-1">
                 {service.services.map((service) => (
                   <li key={service}>
-                    <p>
-                      {service}
-                    </p>
+                    <p>{service}</p>
                   </li>
                 ))}
               </ul>
-            </div>
+            </Link>
           ))}
-          
-
-        </div>
-      )}
+        </nav>
+      ) : null}
     </li>
   );
 };
