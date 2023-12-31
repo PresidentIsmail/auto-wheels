ae0ab5a93f690ba568c5937a77ebe406ed1ed328
Ismail <shaikhnagismail@gmail.com>
2023-12-31 09:57:34 +0200

    feat: Add client-side rendering and scroll animation to Navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
 import Dynamic from "next/dynamic";
+import {
+  Variants,
+  motion,
+  useMotionValueEvent,
+  useScroll,
+} from "framer-motion";
 
 import NavItems from "./NavItems";
 import ContactButton from "./ContactButton";
@@ -9,12 +17,45 @@ const MobileNavItems = Dynamic(() => import("./mobile-nav/MobileNavItems"), {
   ssr: false,
 });
 
+const navbarVariants: Variants = {
+  hidden: {
+    y: "-100%",
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.35,
+      ease: "easeInOut",
+    },
+  },
+};
+
 const Navbar: React.FC = () => {
+  const [isNavbarVisible, setNavbarVisibility] = useState(true);
+  const { scrollY } = useScroll();
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    const previous = scrollY.getPrevious();
+    const delta = latest - previous;
+
+    if (delta > 0 && latest > 100) {
+      setNavbarVisibility(false);
+    } else {
+      setNavbarVisibility(true);
+    }
+  });
+
   return (
-    <nav className="fixed top-0 w-full inset-x-0 z-20">
+    <motion.nav
+      variants={navbarVariants}
+      animate={isNavbarVisible ? "visible" : "hidden"}
+      className="fixed inset-x-0 top-0 z-20 w-full"
+    >
       <HeaderInformation />
 
-      <div className="relative h-navbar-mobile md:h-navbar-desktop bg-special border-b border-b-[#1212125c]">
+      <div className="relative h-navbar-mobile border-b border-b-[#1212125c] bg-special md:h-navbar-desktop">
         <div className="master-container flex h-full items-center justify-between">
           {/* Business Name */}
           <Link
@@ -36,7 +77,7 @@ const Navbar: React.FC = () => {
           <ContactButton />
         </div>
       </div>
-    </nav>
+    </motion.nav>
   );
 };
 
