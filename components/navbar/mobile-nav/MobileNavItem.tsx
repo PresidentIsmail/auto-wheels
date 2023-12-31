a26bf492582b1c456a3c3c63617104100806aa58
Ismail <shaikhnagismail@gmail.com>
2023-12-31 19:28:11 +0200

    feat: Add Image component to MobileNavItem

--- components/navbar/mobile-nav/MobileNavItem.tsx
+++ components/navbar/mobile-nav/MobileNavItem.tsx
@@ -2,19 +2,21 @@
 
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
-import { NAV_ITEMS } from "@/constants";
+import { NAV_ITEMS, SERVICES_DATA } from "@/constants";
 
 import { ChevronRight } from "lucide-react";
 
+
 type LinkItemProps = {
   type: "link";
-  navItem: (typeof NAV_ITEMS)[number];
+  navItem: (typeof NAV_ITEMS)[number] | (typeof SERVICES_DATA)[number];
 };
 
 type ButtonItemProps = {
   type: "button";
-  navItem: (typeof NAV_ITEMS)[number];
+  navItem: (typeof NAV_ITEMS)[number] | (typeof SERVICES_DATA)[number];
   onClick: () => void;
 };
 
@@ -25,9 +27,17 @@ const MobileNavItem: React.FC<MobileNavItemProps> = (props) => {
     return (
       <Link
         href={"#"}
-        className="block w-full p-2 transition-colors hover:bg-white/10 focus-visible:bg-white/10 "
+        className="flex w-full items-center gap-x-1.5 p-2 transition-colors hover:bg-white/10 focus-visible:bg-white/10 "
         aria-label={props.navItem.label}
       >
+        {"icon" in props.navItem && (
+          <Image
+            src={props.navItem.icon as string}
+            alt={props.navItem.label}
+            width={16}
+            height={16}
+          />
+        )}
         {props.navItem.label}
       </Link>
     );
