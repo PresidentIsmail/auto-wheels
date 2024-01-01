217a9772d126460470505691a05e70a2441d6376
Ismail <shaikhnagismail@gmail.com>
2024-01-01 18:47:18 +0200

    Feat: Begin uilding TireSelection.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/navbar/Navbar";
 import Hero from "@/components/hero-section/Hero";
-import Statistics from "@/components/ui/statistic-section/Statistics";
+import Statistics from "@/components/statistic-section/Statistics";
+import TireSelection from "@/components/tire-selection-section/TireSelection";
 
 export default function Home() {
   return (
@@ -8,13 +9,14 @@ export default function Home() {
       <Navbar />
       <main
         className="
+        relative 
         mt-[calc(var(--navbar-mobile-height)+var(--header-info-height))] 
-        flex 
-        min-h-[200vh] flex-col items-center bg-white md:mt-[calc(var(--navbar-desktop-height)+var(--header-info-height))] w-full relative
+        flex min-h-[200vh] w-full flex-col items-center bg-white md:mt-[calc(var(--navbar-desktop-height)+var(--header-info-height))]
       "
       >
         <Hero />
         <Statistics />
+        <TireSelection />
       </main>
     </>
   );
--- components/statistic-section/Statistics.tsx
+++ components/statistic-section/Statistics.tsx
@@ -6,7 +6,10 @@ import Statistic from "./Statistic";
 
 const Statistics: React.FC = () => {
   return (
-    <section className="master-container pt-10" aria-label="Statistics Section">
+    <section
+      className="master-container pb-[72px] pt-10 lg:pb-[120px]"
+      aria-label="Statistics Section"
+    >
       <div className="grid grid-cols-2 gap-x-5 gap-y-8 md:grid-cols-3 md:gap-y-10 lg:grid-cols-5">
         {AUTO_WHEELS_STATS.map((stat) => (
           <Statistic
