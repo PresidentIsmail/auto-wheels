c821298caf6b37d801e14eac090adf470e94cf35
Ismail <shaikhnagismail@gmail.com>
2024-01-03 06:55:28 +0200

    Feat: Add @radix-ui/react-popover dependency and implement TireSizePopover component

--- components/tire-selection-section/TireSelection.tsx
+++ components/tire-selection-section/TireSelection.tsx
@@ -3,36 +3,80 @@ import Image from "next/image";
 
 import tireBg from "@/public/images/tyre-bg-01.webp";
 
+import { Info } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 const TireSelection: React.FC = () => {
   return (
     <section
-      aria-label="Tire Selection Section"
+      aria-label="Section for selecting the tire size for your vehicle"
       className="relative min-h-[500px] w-full pb-24 text-white lg:pb-0"
       style={{
         clipPath: "var(--clip-path)",
       }}
     >
-      {/* background image */}
-      <Image
-        src={tireBg}
-        alt="Tire Selection Background"
-        fill
-        sizes="100vw"
-        quality={70}
-        placeholder="blur"
-        className="absolute left-0 top-0 z-0 h-full w-full object-cover object-center brightness-[25%]"
-      />
-      {/* overlay on background image */}
-      <div className="absolute left-0 top-0 z-0 h-full w-full bg-brand opacity-[5%]" />
+      <BackgroundImage />
+      <Overlay />
 
-      {/* Header */}
-      <header className="master-container relative z-10 pt-6">
-        <h2 className="text-2xl font-extrabold sm:text-3xl md:text-4xl lg:text-5xl">
-          Find the Perfect Tires for Your Vehicle.
-        </h2>
-      </header>
+      {/* Content */}
+      <article className="master-container relative z-10 flex flex-col gap-y-6">
+        {/* Header */}
+        <header className="pt-6">
+          <h2 className="text-2xl font-extrabold sm:text-3xl md:text-4xl lg:text-5xl">
+            Find the Perfect Tires for Your Vehicle.
+          </h2>
+        </header>
+
+        <section className="w-full bg-[#323236]/80 p-2">
+          <h3 className="text-2xl font-bold md:text-3xl">
+            Tell us your tire size
+          </h3>
+
+          <TireSizePopover />
+        </section>
+      </article>
     </section>
   );
 };
+
+const BackgroundImage: React.FC = () => (
+  <Image
+    src={tireBg}
+    alt="Tire Selection Background"
+    fill
+    sizes="100vw"
+    quality={70}
+    placeholder="blur"
+    className="absolute left-0 top-0 z-0 h-full w-full object-cover object-center brightness-[25%]"
+  />
+);
+
+const Overlay: React.FC = () => (
+  <div className="absolute left-0 top-0 z-0 h-full w-full bg-brand opacity-[5%]" />
+);
+
+const TireSizePopover: React.FC = () => (
+  <Popover>
+    <PopoverTrigger
+      className="decoration-white/80 underline-offset-2 hover:underline"
+      aria-haspopup="true"
+      aria-controls="popover-content"
+    >
+      <p className="text-xs font-normal italic text-white/80 ">
+        <span className="inline-block translate-y-[3px]">
+          <Info className="h-4 w-4" />
+        </span>{" "}
+        Need help with your tire size?
+      </p>
+    </PopoverTrigger>
+    <PopoverContent id="popover-content">
+      Place content for the popover here.
+    </PopoverContent>
+  </Popover>
+);
 
 export default TireSelection;
--- components/tire-selection-section/TireSelectionInput.tsx
+++ components/tire-selection-section/TireSelectionInput.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import React from 'react';
-import {useForm, FieldValues} from 'react-hook-form';
 
 const TireSelectionInput: React.FC = () => {
-    const {register, handleSubmit, formState: {errors, isSubmitting}} = useForm<FieldValues>();
 
     return (
         <div>
