5db4ea5d03a3d1179eab0b4e5ccf008f89333ddf
Ismail <shaikhnagismail@gmail.com>
2024-01-04 04:48:40 +0200

    Feat: Update button and add input and label components

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center xl:text-base tracking-wide whitespace-nowrap rounded-full text-sm font-medium ring-offset-brand transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center xl:text-base tracking-wide whitespace-nowrap rounded-full text-sm font-medium ring-offset-brand transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-brand focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-grayBorder bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-grayBorder bg-transparent hover:bg-accent hover:text-accent-foreground",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
