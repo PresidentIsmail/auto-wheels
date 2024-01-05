f80c02ca39a012bf4253ded646a06edf780b4774
Ismail <shaikhnagismail@gmail.com>
2024-01-06 01:01:38 +0200

    Add ErrorMessage component

---
+++ components/tire-selection-section/ErrorMessage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { AlertCircle } from "lucide-react";
+
+interface ErrorMessageProps {
+  message: string;
+}
+
+const ErrorMessage: React.FC<ErrorMessageProps> = ({ message }) => {
+  return (
+    <div className="absolute left-0 mt-1 flex max-w-sm translate-x-1 items-center gap-x-2">
+      <AlertCircle className="h-4 w-4 text-error-600" />
+      <p className="text-xs text-red-500">{message}</p>
+    </div>
+  );
+};
+
+export default ErrorMessage;
