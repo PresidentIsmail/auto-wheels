6e2971381c0f298f35a856b1396122a483fade46
Ismail <shaikhnagismail@gmail.com>
2024-01-06 22:42:36 +0200

    Chore: Update tireData with additional widths and profiles

--- data/TireData.ts
+++ data/TireData.ts
@@ -9,50 +9,64 @@ interface TireSizeData {
 
 export const tireData: TireSizeData = {
   13: {
-    widths: [175, 185, 195],
-    profiles: [60, 65, 70],
-  },
-  14: {
     widths: [175, 185, 195, 205, 215],
-    profiles: [65, 70, 75],
+    profiles: [60, 65, 70, 75, 80],
   },
+
+  14: {
+    widths: [185, 195, 205, 215, 225],
+    profiles: [60, 65, 70, 75, 80],
+  },
+
   15: {
-    widths: [195, 205, 215],
-    profiles: [60, 65, 70],
+    widths: [195, 205, 215, 225, 235],
+    profiles: [60, 65, 70, 75, 80],
   },
+
   16: {
-    widths: [205, 215, 225],
-    profiles: [65, 70, 75],
+    widths: [205, 215, 225, 235, 245],
+    profiles: [55, 60, 65, 70, 75],
   },
+
   17: {
-    widths: [215, 225, 235],
-    profiles: [55, 60, 65],
+    widths: [215, 225, 235, 245, 255],
+    profiles: [50, 55, 60, 65, 70],
   },
+
   18: {
-    widths: [225, 235, 245],
-    profiles: [60, 65, 70],
+    widths: [225, 235, 245, 255, 265],
+    profiles: [45, 50, 55, 60, 65],
   },
+
   19: {
-    widths: [235, 245, 255],
-    profiles: [45, 50, 55],
+    widths: [235, 245, 255, 265, 275],
+    profiles: [40, 45, 50, 55, 60],
   },
+
   20: {
-    widths: [245, 255, 265],
-    profiles: [50, 55, 60],
+    widths: [245, 255, 265, 275, 285],
+    profiles: [35, 40, 45, 50, 55],
   },
+
   21: {
-    widths: [255, 265, 275],
-    profiles: [45, 50, 55],
+    widths: [285, 295],
+    profiles: [45, 50],
   },
 };
 
-// common tire widths
-export const DEFAULT_TIRE_WIDTHS = [
-  155, 165, 175, 185, 195, 205, 215, 225, 235, 245, 255, 265, 275,
-];
+// common tire widths. unique widths from tireData
+let uniqueWidths = new Set<number>();
+for (let tireSize in tireData) {
+  tireData[tireSize].widths.forEach((width) => uniqueWidths.add(width));
+}
+export const DEFAULT_TIRE_WIDTHS = Array.from(uniqueWidths).sort();
 
 // common tire profiles
-export const DEFAULT_TIRE_PROFILES = [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85];
+let uniqueProfiles = new Set<number>();
+for (let tireSize in tireData) {
+  tireData[tireSize].profiles.forEach((profile) => uniqueProfiles.add(profile));
+}
+export const DEFAULT_TIRE_PROFILES = Array.from(uniqueProfiles).sort();
 
 // common tire diameters
-export const DEFAULT_TIRE_DIAMETERS = [13, 14, 15, 16, 17, 18, 19, 20, 21];
+export const DEFAULT_TIRE_DIAMETERS = Object.keys(tireData).sort();
