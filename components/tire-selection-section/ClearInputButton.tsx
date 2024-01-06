5d6e75aa0b7930773dfc14c6be5743497f316851
Ismail <shaikhnagismail@gmail.com>
2024-01-07 00:12:15 +0200

    Refactor: Update ClearInputButton component

--- components/tire-selection-section/ClearInputButton.tsx
+++ components/tire-selection-section/ClearInputButton.tsx
@@ -3,12 +3,13 @@ import React from "react";
 import { X } from "lucide-react";
 import { Button } from "../ui/button";
 
-interface ClearInputButtonProps {
+interface ClearInputButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   handleClearInput: () => void;
 }
 
 const ClearInputButton: React.FC<ClearInputButtonProps> = ({
   handleClearInput,
+  ...props
 }) => {
   return (
     <Button
@@ -18,6 +19,7 @@ const ClearInputButton: React.FC<ClearInputButtonProps> = ({
       variant={"ghost"}
       className="absolute right-1 top-1/2 h-max w-max -translate-y-1/2 p-1.5 hover:bg-white/10 hover:text-white"
       aria-label="Clear input"
+      {...props}
     >
       <X className="h-4 w-4 text-white" />
     </Button>
