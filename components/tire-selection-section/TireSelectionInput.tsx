385c616f14baf2b4f014b60c912f9076c024b298
Ismail <shaikhnagismail@gmail.com>
2024-01-07 05:47:00 +0200

    Feat: Automatically update suggestions based on users input

--- components/tire-selection-section/TireSelectionInput.tsx
+++ components/tire-selection-section/TireSelectionInput.tsx
@@ -1,57 +1,165 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React, { useState, useEffect } from "react";
 
+import type { TireSize } from "@/data/TireData";
 import {
-  tireData,
+  tireSizes,
   DEFAULT_TIRE_WIDTHS,
   DEFAULT_TIRE_PROFILES,
   DEFAULT_TIRE_DIAMETERS,
 } from "@/data/TireData";
 
-import { Search } from "lucide-react";
-import { Label } from "../ui/label";
+import { Search, Loader } from "lucide-react";
 import { Button } from "../ui/button";
 import AutoCompleteInput from "./AutoCompleteInput";
 
-const TIRE_DIAMETER = 14;
-const INITIAL_SUGGESTIONS = tireData[TIRE_DIAMETER].widths;
-
 const TireSelectionInput: React.FC = () => {
+  const [widthInput, setWidthInput] = useState("");
+  const [profileInput, setProfileInput] = useState("");
+  const [diameterInput, setDiameterInput] = useState("");
+  const [submitting, setSubmitting] = useState(false);
 
-  const [inputValue, setInputValue] = useState<string>("");
-  const [suggestions, setSuggestions] = useState<number[]>(DEFAULT_TIRE_WIDTHS);
+  const [widthSuggestions, setWidthSuggestions] =
+    useState<number[]>(DEFAULT_TIRE_WIDTHS);
+  const [profileSuggestions, setProfileSuggestions] = useState<number[]>(
+    DEFAULT_TIRE_PROFILES,
+  );
+  const [diameterSuggestions, setDiameterSuggestions] = useState<number[]>(
+    DEFAULT_TIRE_DIAMETERS,
+  );
 
+  // Filter width suggestions based on entered profile and diameter
+  useEffect(() => {
+    // if the input is empty, show default suggestions
+    if (profileInput === "" && diameterInput === "") {
+      setWidthSuggestions(DEFAULT_TIRE_WIDTHS);
+      return;
+    }
 
-  
+    // Filter width suggestions based on entered profile and diameter
+    const selectedTires = tireSizes.filter((tire) => {
+      const profileMatches =
+        profileInput === "" || tire.profile === parseInt(profileInput);
+      const diameterMatches =
+        diameterInput === "" || tire.diameter === parseInt(diameterInput);
+      return profileMatches && diameterMatches;
+    });
 
-  const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    // Extract unique widths from the selected tires
+    const uniqueFilteredWidths = [
+      ...new Set(selectedTires.map((tire) => tire.width)),
+    ];
+
+    setWidthSuggestions(uniqueFilteredWidths);
+  }, [profileInput, diameterInput]);
+
+  // Filter profile suggestions based on entered width and diameter
+  useEffect(() => {
+    // if the input is empty, show default suggestions
+    if (widthInput === "" && diameterInput === "") {
+      setProfileSuggestions(DEFAULT_TIRE_PROFILES);
+      return;
+    }
+
+    // Filter profile suggestions based on entered width and diameter
+    const selectedTires = tireSizes.filter((tire) => {
+      const widthMatches =
+        widthInput === "" || tire.width === parseInt(widthInput);
+      const diameterMatches =
+        diameterInput === "" || tire.diameter === parseInt(diameterInput);
+      return widthMatches && diameterMatches;
+    });
+
+    // Extract unique profiles from the selected tires
+    const uniqueFilteredProfiles = [
+      ...new Set(selectedTires.map((tire) => tire.profile)),
+    ];
+
+    setProfileSuggestions(uniqueFilteredProfiles);
+  }, [widthInput, diameterInput]);
+
+  // Filter diameter suggestions based on entered width and profile
+  useEffect(() => {
+    // if the input is empty, show default suggestions
+    if (widthInput === "" && profileInput === "") {
+      setDiameterSuggestions(DEFAULT_TIRE_DIAMETERS);
+      return;
+    }
+
+    // Filter diameter suggestions based on entered width and profile
+    const selectedTires = tireSizes.filter((tire) => {
+      const widthMatches =
+        widthInput === "" || tire.width === parseInt(widthInput);
+      const profileMatches =
+        profileInput === "" || tire.profile === parseInt(profileInput);
+      return widthMatches && profileMatches;
+    });
+
+    // Extract unique diameters from the selected tires
+    const uniqueFilteredDiameters = [
+      ...new Set(selectedTires.map((tire) => tire.diameter)),
+    ];
+
+    setDiameterSuggestions(uniqueFilteredDiameters);
+    console.log("filteredDiameters", uniqueFilteredDiameters);
+  }, [widthInput, profileInput]);
+
+  const handleSearch = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(`Searching for width: ${inputValue}`);
+    setSubmitting(true);
+    console.log(
+      `{ width: ${widthInput}, profile: ${profileInput}, diameter: ${14} }`,
+    );
+    setSubmitting(false);
   };
 
- 
-
   return (
-    <form onSubmit={handleSearch} className="relative flex w-full items-end">
-      <div className="grid max-w-sm items-center gap-1.5">
-        <Label htmlFor="width" className="translate-x-[4px]">
-          Width
-        </Label>
-        <AutoCompleteInput 
-          id="width"
-          placeholder="e.g. 205"
-          data={suggestions}
-        />
-      </div>
+    <form
+      onSubmit={handleSearch}
+      className="relative flex w-full items-end gap-x-6"
+    >
+      {/* Width Input */}
+      <AutoCompleteInput
+        inputValue={widthInput}
+        setInputValue={setWidthInput}
+        label="Width"
+        placeholder="e.g. 205"
+        suggestionData={widthSuggestions}
+      />
+
+      {/* Profile Input */}
+      <AutoCompleteInput
+        inputValue={profileInput}
+        setInputValue={setProfileInput}
+        label="Profile"
+        placeholder="e.g. 55"
+        suggestionData={profileSuggestions}
+      />
+
+      {/* Diameter Input */}
+      <AutoCompleteInput
+        inputValue={diameterInput}
+        setInputValue={setDiameterInput}
+        label="Diameter"
+        placeholder="e.g. 16"
+        suggestionData={diameterSuggestions}
+      />
 
       {/* Search Button */}
       <Button
         type="submit"
+        disabled={submitting}
         variant={"outline"}
-        className="ml-12 h-10 rounded-md border-[2px] hover:bg-white/10 hover:text-white"
+        className="ml-8 h-10 w-28 rounded-md border-[2px] hover:bg-white/10 hover:text-white"
       >
-        Search <Search className="ml-2 h-4 w-4 text-brand" />
+        {submitting ? (
+          <Loader className="h-4 w-4 animate-spin" />
+        ) : (
+          <>
+            Search <Search className="ml-2 h-4 w-4 text-brand" />
+          </>
+        )}
       </Button>
     </form>
   );
