55764c6a45116bb60a247d8f4f5f85150e4e003f
Ismail <shaikhnagismail@gmail.com>
2024-01-11 18:14:26 +0200

    Feat: Refactor TestimonialCard component to truncate long comments

--- components/testimonials-section/TestimonialCard.tsx
+++ components/testimonials-section/TestimonialCard.tsx
@@ -25,7 +25,7 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({ testimonial }) => {
     .join("");
 
   return (
-    <Card role="article" className="w-[200px] h-max shrink-0 md:w-[250px]">
+    <Card role="article" className="h-max w-[200px] shrink-0 md:w-[250px]">
       <CardHeader className="px-3 pb-4 pt-4 md:pb-6">
         {/* Stars */}
         <Stars rating={testimonial.rating} />
@@ -33,9 +33,11 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({ testimonial }) => {
 
       <CardContent className="px-3 pb-4 md:pb-6">
         {/* User comment */}
-        <blockquote className="text-xs text-muted-foreground md:text-sm">
-          {testimonial.comment}
-        </blockquote>
+        <q className="text-xs text-muted-foreground md:text-sm">
+          {testimonial.comment.length > 100
+            ? `${testimonial.comment.substring(0, 100)}...`
+            : testimonial.comment}
+        </q>
       </CardContent>
 
       <CardFooter className="px-3 pb-2 md:pb-4">
