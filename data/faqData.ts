f5f9c685d042bf725dab3d1e64db286e9d695b43
Ismail <shaikhnagismail@gmail.com>
2024-01-14 11:41:48 +0200

    Chore: Add FAQ data to faqData.ts

---
+++ data/faqData.ts
@@ -0,0 +1,34 @@
+import { generateId } from "@/lib/helper";
+
+export const faqData = [
+  {
+    id: generateId(),
+    question: "Can I purchase tires from your shop?",
+    answer:
+      "Absolutely! We offer a wide selection of quality tires. Our expert team can help you choose the right tires based on your driving habits, vehicle specifications, and budget. Feel free to ask for personalized recommendations during your visit.",
+  },
+  {
+    id: generateId(),
+    question: "What types of vehicles do you service?",
+    answer:
+      "We specialize in servicing a wide range of vehicles, including cars, trucks, SUVs, and motorcycles, providing comprehensive auto care for diverse needs.",
+  },
+  {
+    id: generateId(),
+    question: "How often should I schedule routine maintenance for my vehicle?",
+    answer:
+      "We recommend regular maintenance every 6 months or as per your vehicle manufacturer's guidelines. This ensures optimal performance, fuel efficiency, and extends the lifespan of your vehicle.",
+  },
+  {
+    id: generateId(),
+    question: "Do you provide warranties on your services?",
+    answer:
+      "Yes, we stand behind our work. Many of our services come with warranties. Feel free to ask our team about specific warranty details for your service.",
+  },
+  {
+    id: generateId(),
+    question: "How can I request an appointment for service?",
+    answer:
+      "Booking an appointment is easy! You can call our service center during business hours, use our online appointment scheduler on our website, or visit us in person. We're here to make servicing your vehicle convenient for you.",
+  },
+];
