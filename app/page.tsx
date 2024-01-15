48376cdea85185f7a8fd769f7a557faabdba8d72
Ismail <shaikhnagismail@gmail.com>
2024-01-15 06:53:02 +0200

    Feat: Add Contact and BrandsSlider components

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import Services from "@/components/services-section/Services";
 import Promotions from "@/components/promotions-section/Promotions";
 import Testimonials from "@/components/testimonials-section/Testimonial";
 import Faq from "@/components/faq-section/Faq";
+import Contact from "@/components/contact-section/Contact";
+import BrandsSlider from "@/components/brands-slider-section/BrandsSlider";
 
 export default function Home() {
   return (
@@ -14,17 +16,23 @@ export default function Home() {
       <main
         className="
         relative 
-        mt-[calc(var(--navbar-mobile-height)+var(--header-info-height))] 
-         min-h-[200vh] w-full bg-white md:mt-[calc(var(--navbar-desktop-height)+var(--header-info-height))]
+        mt-[calc(var(--navbar-mobile-height)+var(--header-info-height))]
+        min-h-[200vh] 
+         w-full overflow-x-hidden bg-white md:mt-[calc(var(--navbar-desktop-height)+var(--header-info-height))]
       "
       >
         <Hero />
         <Statistics />
-        <TireSelection />
+        {/* <TireSelection />
         <Services />
         <Promotions />
-        <Testimonials />
+        <Testimonials /> */}
         <Faq />
+        <Contact />
+        <BrandsSlider />
+
+        {/* area to scroll */}
+        <div className="relative z-[-1] h-[100vh] w-full bg-white"></div>
       </main>
     </>
   );
