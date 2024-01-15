007779c819cd98b7f57bf31c741a715429faa1aa
Ismail Shaikhnag <80391089+PresidentIsmail@users.noreply.github.com>
2024-01-15 10:59:29 +0000

    Task: Add constants for footer item structure

--- constants/index.ts
+++ constants/index.ts
@@ -51,29 +51,6 @@ export const BUSINESS_ADDRESS = "23 Nelson Mandela Dr, Mahikeng";
 export const BUSINESS_ADDRESS_ON_GOOGLE_MAPS =
   "https://www.google.com/maps/place/Auto+Wheels+%26+Exhausts/@-25.8632276,25.6459493,17z/data=!3m1!4b1!4m6!3m5!1s0x1ea2cac697680bb7:0x2ad30578d491e15a!8m2!3d-25.8632276!4d25.6459493!16s%2Fg%2F1pzr6fdt5?entry=ttu";
 
-type NavItem = {
-  label: string;
-  href: string;
-};
-
-export const NAV_ITEMS: NavItem[] = [
-  {
-    label: "Promotions",
-    href: "#",
-  },
-  {
-    label: "Tires",
-    href: "#",
-  },
-  {
-    label: "Batteries",
-    href: "#",
-  },
-  {
-    label: "Services",
-    href: "#",
-  },
-];
 
 type Service = {
   label: string;
@@ -197,3 +174,49 @@ export const AUTO_WHEELS_STATS = [
     value: 200,
   },
 ];
+
+// =========== NAVBAR ================
+type NavItem = {
+  label: string;
+  href: string;
+};
+
+export const NAV_ITEMS: NavItem[] = [
+  {
+    label: "Promotions",
+    href: "#",
+  },
+  {
+    label: "Tires",
+    href: "#",
+  },
+  {
+    label: "Batteries",
+    href: "#",
+  },
+  {
+    label: "Services",
+    href: "#",
+  },
+];
+
+// =========== FOOTER ================
+type FooterItem = {
+  title: string;
+  links: string[];
+}
+
+export const FOOTER_ITEMS: FooterItem[] = [
+  {
+    title: "Products",
+    links: ["tires", "batteries", "wipers"],
+  },
+  {
+    title: "Popular Services",
+    links: ["suspension", "exhaust", "wheel alignment"],
+  },
+  {
+    title: "Comapny",
+    links: ["about us", "contact us"],
+  },
+]
