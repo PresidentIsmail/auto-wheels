140d0228bb6dc1cb6f28028fa330d3cc521c0e74
Ismail Shaikhnag <80391089+PresidentIsmail@users.noreply.github.com>
2024-01-16 15:06:06 +0000

    Feat: Add function to generate whatsapp link

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
+import { BUSINESS_TELEPHONE_NUMBER } from "@/constants";
 
 export function extractNumericPart(inputString: string): number | null {
   const numericMatch = inputString.match(/\d+/);
@@ -12,3 +13,11 @@ export function extractNumericPart(inputString: string): number | null {
 export function generateId(): string {
   return uuidv4();
 }
+
+// Function to generate WhatsApp Click to Chat URL
+const generateWhatsAppLink = (phoneNumber: string = BUSINESS_TELEPHONE_NUMBER as string, message: string = ""): string => {
+  const baseUrl = "https://wa.me/";
+  const fullPhoneNumber = phoneNumber.replace(/\D/g, ''); // Remove non-numeric characters
+  const encodedMessage = encodeURIComponent(message);
+  return `${baseUrl}${fullPhoneNumber}?text=${encodedMessage}`;
+};
