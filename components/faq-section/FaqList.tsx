99e179deefbd83c6e21213bb3028525988442c9f
Ismail <shaikhnagismail@gmail.com>
2024-01-19 04:22:59 +0200

    Style:e SlideInContent with slideUp animation

--- components/faq-section/FaqList.tsx
+++ components/faq-section/FaqList.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { faqData } from "@/data/faqData";
-import SlideInContent from "../SlideInContent";
 
 import {
   Accordion,
@@ -10,12 +9,13 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import FaqItem from "./FaqItem";
+import SlideInContent from "../SlideInContent";
 
 const FaqList: React.FC = () => {
   return (
     <Accordion type="single" collapsible className="w-full lg:w-[60%]">
       {faqData.map((faq, index) => (
-        <SlideInContent key={faq.id} delay={index * 0.5} direction="slideDown">
+        <SlideInContent key={faq.id} delay={index * 0.25} direction="slideUp">
           <FaqItem
             value={index.toString()}
             question={faq.question}
