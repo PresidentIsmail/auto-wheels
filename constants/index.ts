3687f390c85ea75f6be3e021865ca823df52d9a6
Ismail <shaikhnagismail@gmail.com>
2024-01-20 21:26:43 +0200

    Task: Refactor video and image paths, update heroMedia export

--- constants/index.ts
+++ constants/index.ts
@@ -1,10 +1,25 @@
 // =========== HERO BACKGROUND VIDEO AND POSTER ================
-export const HERO_MP4_MOBILE_PATH = "./videos/mp4/promo-01-mobile.mp4";
-export const HERO_MP4_DESKTOP_PATH = "./videos/mp4/promo-01-desktop.mp4";
-export const HERO_WEBM_MOBILE_PATH = "./videos/webm/promo-01-mobile.webm";
-export const HERO_WEBM_DESKTOP_PATH = "./videos/webm/promo-01-desktop.webm";
-export const HERO_POSTER_MOBILE_PATH =  "./videos/posters/mobile-poster-01.png";
-export const HERO_POSTER_DESKTOP_PATH = "./videos/posters/desktop-poster-01.png";
+const HERO_MP4_MOBILE_PATH = "./videos/mp4/promo-01-mobile.mp4";
+const HERO_MP4_DESKTOP_PATH = "./videos/mp4/promo-01-desktop.mp4";
+const HERO_WEBM_MOBILE_PATH = "./videos/webm/promo-01-mobile.webm";
+const HERO_WEBM_DESKTOP_PATH = "./videos/webm/promo-01-desktop.webm";
+import HERO_POSTER_MOBILE from "@/public/videos/posters/mobile-poster-01.png";
+import HERO_POSTER_DESKTOP from "@/public/videos/posters/desktop-poster-01.png";
+// Exporting the imported images and videos
+export const heroMedia = {
+  mp4: {
+    mobile: HERO_MP4_MOBILE_PATH,
+    desktop: HERO_MP4_DESKTOP_PATH,
+  },
+  webm: {
+    mobile: HERO_WEBM_MOBILE_PATH,
+    desktop: HERO_WEBM_DESKTOP_PATH,
+  },
+  poster: {
+    mobile: HERO_POSTER_MOBILE,
+    desktop: HERO_POSTER_DESKTOP,
+  },
+};
 
 // =========== BRANDS ================
 import bfgoodrich from "@/public/images/brands/BFGoodrich.png";
@@ -60,7 +75,6 @@ export const BUSINESS_WORKING_HOURS = [
 export const BUSINESS_ADDRESS = "23 Nelson Mandela Dr, Mahikeng";
 export const BUSINESS_ADDRESS_ON_GOOGLE_MAPS =
   "https://www.google.com/maps/place/Auto+Wheels+%26+Exhausts/@-25.8632276,25.6459493,17z/data=!3m1!4b1!4m6!3m5!1s0x1ea2cac697680bb7:0x2ad30578d491e15a!8m2!3d-25.8632276!4d25.6459493!16s%2Fg%2F1pzr6fdt5?entry=ttu";
-
 
 // =========== SERVICES ================
 type Service = {
@@ -163,7 +177,6 @@ export const SERVICES_DATA_SORTED = SERVICES_DATA.sort((a, b) =>
   a.label.localeCompare(b.label),
 );
 
-
 // =========== AUTO WHEELS STATS ================
 export const AUTO_WHEELS_STATS = [
   {
@@ -187,7 +200,6 @@ export const AUTO_WHEELS_STATS = [
     value: 200,
   },
 ];
-
 
 // =========== NAVBAR ================
 type NavItem = {
