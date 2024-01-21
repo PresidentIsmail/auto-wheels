3a7ba1e343b13e3a8e1842a19280253a90181c1d
Ismail <shaikhnagismail@gmail.com>
2024-01-21 23:51:46 +0200

    Refactor: Update video poster paths

--- constants/index.ts
+++ constants/index.ts
@@ -3,8 +3,8 @@ const HERO_MP4_MOBILE_PATH = "/videos/mp4/promo-01-mobile.mp4";
 const HERO_MP4_DESKTOP_PATH = "/videos/mp4/promo-01-desktop.mp4";
 const HERO_WEBM_MOBILE_PATH = "/videos/webm/promo-01-mobile.webm";
 const HERO_WEBM_DESKTOP_PATH = "/videos/webm/promo-01-desktop.webm";
-import HERO_POSTER_MOBILE from "@/public/videos/posters/mobile-poster-01.png";
-import HERO_POSTER_DESKTOP from "@/public/videos/posters/desktop-poster-01.png";
+import HERO_POSTER_MOBILE from "@/public/videos/posters/poster-01.png";
+import HERO_POSTER_DESKTOP from "@/public/videos/posters/poster-01.png";
 // Exporting the imported images and videos
 export const heroMedia = {
   mp4: {
@@ -75,7 +75,6 @@ export const BUSINESS_WORKING_HOURS = [
 export const BUSINESS_ADDRESS = "23 Nelson Mandela Dr, Mahikeng";
 export const BUSINESS_ADDRESS_ON_GOOGLE_MAPS =
   "https://www.google.com/maps/place/Auto+Wheels+%26+Exhausts/@-25.8632276,25.6459493,17z/data=!3m1!4b1!4m6!3m5!1s0x1ea2cac697680bb7:0x2ad30578d491e15a!8m2!3d-25.8632276!4d25.6459493!16s%2Fg%2F1pzr6fdt5?entry=ttu";
-
 
 // =========== AUTO WHEELS STATS ================
 export const AUTO_WHEELS_STATS = [
