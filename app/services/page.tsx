ce0144a3e8ec65070840d6bdc26319d39879d6d2
Ismail <shaikhnagismail@gmail.com>
2024-01-21 16:05:18 +0200

    Feat: Add Services page component

---
+++ app/services/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Services() {
+  return (
+    <React.Fragment>
+      <div className="container mx-auto px-4">
+        <h1 className="text-center text-4xl font-bold">Services</h1>
+      </div>
+    </React.Fragment>
+  );
+}
